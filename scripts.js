46c738ea61539bbec226994a22b690e9594d781f
Adam Rice <adamcoltonrice@gmail.com>
2017-01-29 19:03:32 -0700

    add swiping & transform willChange to individual card

--- scripts.js
+++ scripts.js
@@ -9,22 +9,56 @@ class Cards {
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
     this.update = this.update.bind(this);
+    this.target = null;
+    this.startX = 0;
+    this.currentX = 0;
+
+    this.addEventListeners();
+
+    requestAnimationFrame(this.update);
   }
 
-  onStart() {
+  addEventListeners() {
+    document.addEventListener('touchstart', this.onStart);
+    document.addEventListener('touchmove', this.onMove);
+    document.addEventListener('touchend', this.onEnd);
+  }
+
+  onStart(e) {
+    if(!e.target.classList.contains('card'))
+      return;
+
+    this.target = e.target;
+    this.startX = e.pageX || e.touches[0].pageX;
+    this.currentX = this.startX;
+    this.target.style.willChange = 'transform';
+
+    e.preventDefault();
 
   }
 
-  onMove() {
+  onMove(e) {
+    if(!this.target) {
+      return;
 
+      this.currentX = e.pageX || e.touches[0].pageX;
+    }
   }
 
-  onEnd() {
-
+  onEnd(e) {
+    if(!this.target)
+      return;
   }
 
   update() {
+    requestAnimationFrame(this.update);
 
+    if(!this.target)
+    return;
+
+    const screenX = this.currentX - this.startX;
+
+    this.target.style.transform = `translateX(${screenX}px)`;
   }
 }
 
